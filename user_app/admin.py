d02799c66074029e960c61638e95286e01b1a480
khypiao <kanghaiyan1027@163.com>
2018-09-21 23:34:49 +0800

    add test_platform

---
+++ user_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# 后台管理系统
---
+++ user_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# ORM Object Relational Mapping 创建数据库表的
+
+# table_name.objects.all()
