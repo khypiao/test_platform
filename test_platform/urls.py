02d0dc31300d6551027a723329d123144d957c3d
khypiao <kanghaiyan1027@163.com>
2018-09-26 23:14:46 +0800

    modify login page style

--- test_platform/urls.py
+++ test_platform/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('login_action/', views.login_action),
+
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import auth
 
 # Create your views here.
 # 主要代码逻辑
@@ -10,10 +11,29 @@ def index(request):
 
 
 def login_action(request):
-    if request.method == 'GET':
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        # print(username)
+        # print(isinstance(username, None))
+        # print(type(username))
+        # print(type(""))
 
         if username == "" or password == "":
             return render(request, "index.html", {"error": "用户名或密码为空"})
-        return render(request, "index.html")
+        else:
+            user = auth.authenticate(username=username, password=password)
+            print(user)
+            print(type(user))
+            if user is not None:
+                auth.login(request, user)
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html", {"error": "用户名或密码错误"})
+
+        # if username == "admin" and password == "123456":
+        #     return render(request, "project_manage.html")
+        # else:
+        #     return render(request, "index.html", {"error": "用户名或密码错误"})
+
+        # return render(request, "index.html")
