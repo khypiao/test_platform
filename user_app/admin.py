5637c7c9aeeff149b6205dc8d0aa2681c0994c97
kanghaiyan <kanghaiyan@ehomepay.com.cn>
2018-10-21 22:31:44 +0800

    add more file

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from user_app.models import Project, Module
 
 # Register your models here.
 # 后台管理系统
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'describe', 'status', 'create_time', 'id']
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'describe', 'create_time', 'project', 'id']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Module, ModuleAdmin)
--- user_app/views.py
+++ user_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from user_app.models import Project
 
 # Create your views here.
 # 主要代码逻辑
-#
+
 
 def index(request):
     return render(request, "index.html")
@@ -14,26 +16,32 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")
-        # print(username)
-        # print(isinstance(username, None))
-        # print(type(username))
-        # print(type(""))
 
         if username == "" or password == "":
             return render(request, "index.html", {"error": "用户名或密码为空"})
         else:
-            user = auth.authenticate(username=username, password=password)
-            print(user)
-            print(type(user))
+            user = auth.authenticate(username=username, password=password)  # 验证用户是否存在
+
             if user is not None:
-                auth.login(request, user)
-                return render(request, "project_manage.html")
+                auth.login(request, user)  # 记录用户登录状态， 与login_required配合使用
+                request.session['user'] = username
+                return HttpResponseRedirect("/project_manage/")
             else:
                 return render(request, "index.html", {"error": "用户名或密码错误"})
 
-        # if username == "admin" and password == "123456":
-        #     return render(request, "project_manage.html")
-        # else:
-        #     return render(request, "index.html", {"error": "用户名或密码错误"})
 
-        # return render(request, "index.html")
+@login_required  # 判断用户是否登录
+def project_manage(request):
+    username = request.session.get('user')
+    project_all = Project.objects.all()
+    print(project_all)
+    return render(request, "project_manage.html", {
+        "user": username,
+        "projects": project_all
+    })
+
+
+def logout(request):
+    auth.logout(request)  # 清除用户登录状态
+    response = HttpResponseRedirect('/')
+    return response
